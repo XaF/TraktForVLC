e6b83bf829dd178ed240e9404f2c622291e056ae
Raphaël Beamonte <raphael.beamonte@gmail.com>
2014-07-30 18:08:48 -0400

    movie_info: change the way get_movie_info works to match more accurately
    Before this patch, get_movie_info was just searching for an exact match
    in the title, without even looking if the result was a movie or a series.
    With this patch, if there is no result at first or if the result is a
    tv show, get_movie_info will use the omdbapi to search for results and,
    if there are some, order the results by proximity with the request before
    using the best match (if proximity >= 90%). This allows to have some
    errors in the file name.
    
    Signed-off-by: Raphaël Beamonte <raphael.beamonte@gmail.com>

--- movie_info.py
+++ movie_info.py
@@ -1,51 +1,124 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (C) 2014        Raphaël Beamonte
+#
+# This file is part of TraktForVLC.  TraktForVLC is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+# or see <http://www.gnu.org/licenses/>.
+
+
 import re
 import requests
 import urllib
 import json
+import difflib
 
+# The base url to do the imdb requests to
 BASE_URL = 'http://www.imdbapi.com/?'
-#NAME_LIST = file('movies.txt','r')
+
+# The list of information we want to extract from the requests results
+info_list = [
+        'Plot',
+        'Title',
+        'Director',
+        'tomatoRating',
+        'imdbRating',
+        'Runtime',
+        'Year'
+        ]
+
 
 def get_movie_info(movi_name, movi_year = ''):
+    # Formatting query to send
     query = {
             'i': '',
             't': movi_name,
             'y': movi_year,
-            'tomatoes':'true'
+            'tomatoes': 'true'
         }
+
+    # Searching for the movie directly
     part = urllib.urlencode(query)
     url = BASE_URL+part
     response = requests.get(url)
-    output  = json.dumps(response.content, separators=(',',':'))
+
+    # Parsing json
+    output = json.loads(response.content)
+
+    # If we don't have a direct match, or if our direct match isn't a movie
+    if (('Response' in output.keys() and output['Response'] == 'False')
+            or output['Type'] != 'movie'):
+        # We set a variable to indicate we didn't found the movie yet
+        found = False
+
+        # We didn't find the movie directly, try to 'search' it
+        querySearch = {
+                'i': '',
+                's': movi_name,
+                'y': movi_year
+            }
+
+        # Sending the research
+        part = urllib.urlencode(querySearch)
+        responseSearch = requests.get(BASE_URL+part)
+
+        # Parsing json
+        results = json.loads(responseSearch.content)
+
+        # If we have results
+        if 'Search' in results.keys():
+            # We need first to filter the results to keep only the movies
+            filtered = [i for i in results['Search'] if i['Type'] == 'movie']
+
+            # If we had at least a movie in the list
+            if filtered:
+                # We sort it by similarity with the request
+                bests = sorted(filtered,
+                        key=lambda x: difflib.SequenceMatcher(None, x['Title'], movi_name).ratio(),
+                        reverse=True
+                        )
+
+                # We select the most similar
+                selected = bests[0]
+
+                # And if its similarity is at least 90%, we act as if it's the movie
+                if difflib.SequenceMatcher(None, selected['Title'], movi_name).ratio() >= 0.9:
+                    found = True
+
+                    # We make a new request using the imdbID of the found movie
+                    # to load all of the information we wanted
+                    query = {
+                            'i': selected['imdbID'],
+                            'tomatoes': 'true'
+                            }
+
+                    part = urllib.urlencode(query)
+                    response = requests.get(BASE_URL+part)
+
+                    # And we parse the new json
+                    output = json.loads(response.content)
+
+        # If we didn't find the movie, we raise an error
+        if not found:
+            raise LookupError("%s: %s" % (movi_name, results['Error']))
+
+    # If we have found the movie, we organize its data to return it
     movie_info = {}
-    info_list = [
-            'Plot',
-            'Title',
-            'Director',
-            'tomatoRating',
-            'imdbRating',
-            'Runtime',
-            'Year'
-        ]
     for info in info_list:
-        if info == 'imdbRating':
-            movie_info['IMDB Rating'] = get_and_clean_data(info, output)
-        movie_info[info] = get_and_clean_data(info, output)
+        if info in output.keys():
+            movie_info[info] = output[info]
+        else:
+            movie_info[info] = None
+
     return movie_info
-
-def get_and_clean_data(tag, data):
-    try:
-        temp_data = data.split(tag)[1].split(",")[0]
-        data = re.sub(r':\\"+','',temp_data).replace('\\"','')
-    except IndexError,e:
-        print "Error Occured! %s" %e
-        import traceback # For debugging purposes
-        traceback.print_exc()
-        return ""	
-    return data	
-
-#def get_movi_name(name_list):
-#    for name in name_list:
-#        print "Getting Movi %s " % name
-#        print get_movie_info(name)
-#    return
