9686bb8270fc30ce5195474e43891b8f9483f041
Raphaël Beamonte <raphael.beamonte@gmail.com>
2014-07-29 20:43:25 -0400

    Add licence information at the top of the file
    Signed-off-by: Raphaël Beamonte <raphael.beamonte@gmail.com>

--- TraktForVLC.py
+++ TraktForVLC.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 # encoding: utf-8
+#
+# TraktForVLC, to link VLC watching to trakt.tv updating
+#
+# Copyright (C) 2012        quietcore
+# Copyright (C) 2013        Damien Battistella
+# Copyright (C) 2014        Raphaël Beamonte
+#
+# This file is part of TraktForVLC.  TraktForVLC is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+# or see <http://www.gnu.org/licenses/>.
 
 #import urllib2
 import logging
