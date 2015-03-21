5ce11e78f7bfc4a521f289f3455c6877aacc8f12
Raphaël Beamonte <raphael.beamonte@gmail.com>
2015-03-20 22:36:24 -0400

    VLCRemote: add header
    Signed-off-by: Raphaël Beamonte <raphael.beamonte@gmail.com>

--- vlcrc/__init__.py
+++ vlcrc/__init__.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# TraktForVLC, to link VLC watching to trakt.tv updating
+#
+# Copyright (C) 2012        Chris Maclellan <chrismaclellan@gmail.com>
+# Copyright (C) 2014-2015   Raphaël Beamonte <raphael.beamonte@gmail.com>
+#
+# This file is part of TraktForVLC.  TraktForVLC is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+# or see <http://www.gnu.org/licenses/>.
+
 import telnetlib
 import re
 import logging
